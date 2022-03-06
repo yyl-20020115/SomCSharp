932e0ed9d344e4bd982c956e50ddf97e488609e5
john2015 <yyl_20050115@hotmail.com>
2022-03-07 02:39:24 +0800

    exact version used to make sure no warp around

--- SomCSharp/vmobjects/SInteger.cs
+++ SomCSharp/vmobjects/SInteger.cs
@@ -63,6 +63,47 @@ public class SInteger : SNumber
         return result == Math.Round(result) ? IntOrBigInt(result, universe) : universe.NewDouble(result);
     }
 
+    public static bool AddExact(int var0, int var1, out int var2)
+    {
+        var2 = var0 + var1;
+        return !(((var0 ^ var2) & (var1 ^ var2)) < 0);
+    }
+
+    public static bool AddExact(long var0, long var2, out long var4)
+    {
+        var4 = var0 + var2;
+        return !(((var0 ^ var4) & (var2 ^ var4)) < 0);
+    }
+
+    public static bool SubtractExact(int var0, int var1, out int var2)
+    {
+        var2 = var0 - var1;
+        return !(((var0 ^ var1) & (var0 ^ var2)) < 0);
+    }
+
+    public static bool SubtractExact(long var0, long var2,out long var4)
+    {
+        var4 = var0 - var2;
+        return !(((var0 ^ var2) & (var0 ^ var4)) < 0L);
+    }
+
+    public static bool MultiplyExact(int var0, int var1, out long var2)
+    {
+        var2 = (long)var0 * (long)var1;
+        return !((long)((int)var2) != var2);
+    }
+
+    public static bool MultiplyExact(long var0, long var2, out long var4)
+    {
+        var4 = var0 * var2;
+        long var6 = Math.Abs(var0);
+        long var8 = Math.Abs(var2);
+        return ((var6 | var8) >> 31 == 0L || (var2 == 0L || var4 / var2 == var0) && (var0 != -9223372036854775808L || var2 != -1L));
+    }
+
+
+
+
     public override SNumber PrimAdd(SNumber right, Universe universe)
     {
         if (right is SBigInteger s)
@@ -76,11 +117,11 @@ public class SInteger : SNumber
         }
         else if(right is SInteger r)
         {
-            try
+            if(AddExact(embeddedInteger,r.embeddedInteger,out var result))
             {
-                return universe.NewInteger(embeddedInteger + r.EmbeddedInteger);
+                return universe.NewInteger(result);
             }
-            catch
+            else
             {
                 return universe.NewBigInteger(new BigInteger(embeddedInteger) + (
                     new BigInteger(r.EmbeddedInteger)));
@@ -103,11 +144,11 @@ public class SInteger : SNumber
         }
         else if (right is SInteger r)
         {
-            try
+            if(SubtractExact(embeddedInteger,r.EmbeddedInteger, out var result))
             {
-                return universe.NewInteger(embeddedInteger - r.EmbeddedInteger);
+                return universe.NewInteger(result);
             }
-            catch
+            else
             {
                 return universe.NewBigInteger(new BigInteger(embeddedInteger) - (
                     new BigInteger(r.EmbeddedInteger)));
@@ -130,11 +171,11 @@ public class SInteger : SNumber
         }
         else if (right is SInteger r)
         {
-            try
+            if(MultiplyExact(embeddedInteger,r.EmbeddedInteger, out var result))
             {
-                return universe.NewInteger(embeddedInteger * r.EmbeddedInteger);
+                return universe.NewInteger(result);
             }
-            catch
+            else
             {
                 return universe.NewBigInteger(new BigInteger(embeddedInteger) * (
                     new BigInteger(r.EmbeddedInteger)));
